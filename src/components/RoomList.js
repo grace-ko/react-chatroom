7163dcfbb068fbbd4c61387fe5407fc39ca7207c
grace-ko <gracekodes@gmail.com>
2019-01-02 19:23:28 -0800

    added create room

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -4,7 +4,8 @@ class RoomList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      rooms: []
+      rooms: [],
+      newRoomName: ''
     };
     this.roomsRef = this.props.firebase.database().ref('rooms');
   }
@@ -17,11 +18,28 @@ class RoomList extends Component {
     });
   }
 
+  handleChange(e) {
+    this.setState({ newRoomName: e.target.value });
+  }
+  createRoom(e) {
+    e.preventDefault();
+    this.roomsRef.push( {
+      name: this.state.newRoomName
+    });
+    document.getElementById('create-room').reset();
+  }
+
   render() {
     return (
       <div>
         <h1>Bloc Chat</h1>
         <div>{this.state.rooms.map(data => <p key={data.key}>{data.name}</p>)}</div>
+        <form id="create-room" onSubmit={(e) => this.createRoom(e)}>
+          <label>Create a Room
+            <input value={this.state.newRoom} onChange={(e) => this.handleChange(e)}/>
+          </label>
+          <input type="submit" />
+        </form>
       </div>
     );
   }
